ccea21aa26f8e8465014afcf2aaad9cad96be795
luyikk <luyikk@126.com>
2019-07-28 11:04:25 +0800

    add

--- ISFSrv/IFSActorController.cs
+++ ISFSrv/IFSActorController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace IFSServ
 {
+    //12311
     [ActorOption(maxQueueCount: 1500, ideltime: 3000)]
     public class IFSActorController : ActorController, IFileActor
     {
